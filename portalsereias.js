622266e8b9247f35483c6a06e8e0e28b4fef891b
Guil913 <guilhermecoelho913@gmail.com>
2024-06-03 01:31:48 +0100

    final ss

--- portalsereias.js
+++ portalsereias.js
@@ -1,6 +1,11 @@
 let capture;
 let threshold = 0.5; 
+let overlayImage;
 let button;
+
+function preload() {
+  overlayImage = loadImage("art/p5 (3).png");
+}
 
 function setup() {
   createCanvas(1550, 863);
@@ -8,9 +13,8 @@ function setup() {
   capture.size(width, height);
   capture.hide(); 
 
- 
   button = createButton('Sereias');
-  button.position(width / 2 - 100, height / 2);
+  positionButton();
   button.size(200, 50);
   button.mousePressed(goToBruxas);
   button.hide(); 
@@ -22,47 +26,59 @@ function draw() {
 
   let orangePixels = 0;
 
-
   capture.loadPixels();
   
- 
   for (let i = 0; i < capture.pixels.length; i += 4) {
     let r = capture.pixels[i];
     let g = capture.pixels[i + 1];
     let b = capture.pixels[i + 2];
     
-  
     let hsl = rgbToHsl(r, g, b);
     let hue = hsl[0]; 
 
-   
     if ((hue >= 20 && hue <= 40) || (hue >= 320 && hue <= 360)) {
       orangePixels++;
     }
   }
 
- 
   let orangePercentage = orangePixels / (capture.width * capture.height);
 
- 
+  // Display the percentage in the middle of the screen
   fill(0);
-  textSize(20);
-  text("Percentagem de Laranja: " + nf(orangePercentage * 100, 2, 2) + "%", 20, 30);
+  textSize(50);
+  textAlign(CENTER, CENTER);
+  text(nf(orangePercentage * 100, 2, 2) + "%", width / 2, height / 2);
 
- 
   if (orangePercentage >= threshold) {
-   
     button.show();
   } else {
-   
     button.hide();
   }
+
+  // Draw overlay image
+  image(overlayImage, 0, 0, width, height);
+
+  // Calculate opacity based on orange percentage
+  let orangeTintOpacity = orangePercentage * 255;
+
+  // Apply orange tint over everything
+  fill(255, 165, 0, orangeTintOpacity);
+  rect(0, 0, width, height);
 }
 
 function goToBruxas() {
   window.location.href = "sereias.html";
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  capture.size(width, height);
+  positionButton();
+}
+
+function positionButton() {
+  button.position(width / 2 - 100, height / 2 + 50);
+}
 
 function rgbToHsl(r, g, b) {
   r /= 255;
